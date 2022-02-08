52141b49a8f1d94cb05939637bd86c4b193fdea5
skerdi <s.berberi@avanade.com>
2022-02-08 20:46:16 +0100

    refactored

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 var answersDict = new Dictionary<string, string>()
 {
-    { "Token_2", "3" }
+    { "Token_2", "99" }
 };
 
 string testinput = "(Token_2 == \"2\") ? OK : (Token_2 == \"99\") ? ERROR : (Token_2 == \"100\") ? WRONG : WARNING";
@@ -10,36 +10,23 @@ Console.WriteLine(Parse(testinput, answersDict));
 
 static string Parse(string input, Dictionary<string, string> answers)
 {
-    var tokenExpressions = input
-                        .Split(new char[] { '(', ')', '?' })
-                        .Where(x => !string.IsNullOrWhiteSpace(x))
-                        .Select(x => x.Trim())
-                        .ToArray();
+    string[] tokenExpressions = TrimCollection(input.Split(new char[] { '(', ')', '?' })); ;
 
     for (int i = 0; i < tokenExpressions.Length - 1; i += 2)
     {
-        var mainBooleanExpression = tokenExpressions[i];
-        var messageExpression = tokenExpressions[i + 1];
+        string mainBooleanExpression = tokenExpressions[i];
+        string messageExpression = tokenExpressions[i + 1];
 
-        string[] expressions = Regex
-                        .Split(mainBooleanExpression, @"\s+")
-                        .Where(x => !string.IsNullOrWhiteSpace(x))
-                        .Select(x => x.Trim())
-                        .ToArray();
-
-        var messages = messageExpression
-                        .Split(':')
-                        .Where(x => !string.IsNullOrWhiteSpace(x))
-                        .Select(x => x.Trim())
-                        .ToArray();
+        string[] expressions = TrimCollection(Regex.Split(mainBooleanExpression, @"\s+"));
+        string[] messages = TrimCollection(messageExpression.Split(':'));
 
         var left = expressions[0];
         var csharpOperator = expressions[1];
         var right = expressions[2];
 
         // removing double quotes from answer in input expression
-        var answer = string.Concat(right.Split('"').Where(x => !string.IsNullOrWhiteSpace(x)));
-
+        var answer = string.Concat(TrimCollection(right.Split('"')));
+        
         if (csharpOperator == "==" && answers[left] == answer)
         {
             return messages[0];
@@ -55,3 +42,8 @@ static string Parse(string input, Dictionary<string, string> answers)
 
     throw new Exception("Couldn't parse the ternary expression");
 }
+
+static string[] TrimCollection(IEnumerable<string> collection)
+    => collection.Where(x => !string.IsNullOrWhiteSpace(x))
+                 .Select(x => x.Trim())
+                 .ToArray();
